05c540737cfe3a8f7d8d58e68ab1c08c0034c494
hyeongminoh <mihn0621@gmail.com>
2023-05-18 08:30:17 +0900

    20230518

--- ch3/src/test/java/com/code/design/object/AccountTest.java
+++ ch3/src/test/java/com/code/design/object/AccountTest.java
@@ -10,70 +10,70 @@ class AccountTest {
     @Test
     public void Account_account_holder_비어있으면_exception() {
         thenThrownBy(() -> Account.builder()
-            .accountHolder("")
-            .bankName("KB")
-            .accountNumber("11010101010")
-            .build()
+                .accountHolder("")
+                .bankName("KB")
+                .accountNumber("11010101010")
+                .build()
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void Account_account_holder_null_exception() {
         thenThrownBy(() -> Account.builder()
-            .accountHolder(null)
-            .bankName("KB")
-            .accountNumber("11010101010")
-            .build()
+                .accountHolder(null)
+                .bankName("KB")
+                .accountNumber("11010101010")
+                .build()
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void Account_bank_name_비어있으면_exception() {
         thenThrownBy(() -> Account.builder()
-            .accountHolder("yun")
-            .bankName("")
-            .accountNumber("11010101010")
-            .build()
+                .accountHolder("ohm")
+                .bankName("")
+                .accountNumber("11010101010")
+                .build()
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void Account_bank_name_null_exception() {
         thenThrownBy(() -> Account.builder()
-            .accountHolder("yun")
-            .bankName(null)
-            .accountNumber("11010101010")
-            .build()
+                .accountHolder("yun")
+                .bankName(null)
+                .accountNumber("11010101010")
+                .build()
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void Account_account_number_비어있으면_exception() {
         thenThrownBy(() -> Account.builder()
-            .accountHolder("yun")
-            .bankName("KB")
-            .accountNumber("")
-            .build()
+                .accountHolder("yun")
+                .bankName("KB")
+                .accountNumber("")
+                .build()
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void Account_account_number_null_exception() {
         thenThrownBy(() -> Account.builder()
-            .accountHolder("yun")
-            .bankName("KB")
-            .accountNumber(null)
-            .build()
+                .accountHolder("yun")
+                .bankName("KB")
+                .accountNumber(null)
+                .build()
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void Account_모든_필수값을_입력_하면_성공() {
         final Account account = Account.builder()
-            .accountHolder("yun")
-            .bankName("KB")
-            .accountNumber("11010101010")
-            .build();
+                .accountHolder("yun")
+                .bankName("KB")
+                .accountNumber("11010101010")
+                .build();
 
         then(account.getAccountHolder()).isEqualTo("yun");
         then(account.getBankName()).isEqualTo("KB");
---
+++ ch3/src/test/java/com/code/design/object/Refund.java
@@ -0,0 +1,2 @@
+@Entity
+@Table(name = "refund")
